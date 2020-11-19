349dbb9ea94a96ee1838fc2019e7dca659485a36
Aditya Bhatraju <48351237+adibns@users.noreply.github.com>
2020-11-19 19:25:20 +0530

    Added optional positional argument
    It is printed in the output as expected

--- demo.py
+++ demo.py
@@ -1,16 +1,16 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Aditya
+# date: 2020-11-19
 
-"""This script prints out docopt args.
-Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
-
+"This script prints out docopt args.
+Usage: demo.R <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-"""
+" -> doc
 
-from docopt import docopt
-opt = docopt(__doc__)
+library(docopt)
+opt <- docopt(doc)
 print(opt)
-print(type(opt))
+print(typeof(opt))
