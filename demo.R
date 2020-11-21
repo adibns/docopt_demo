a79a8710dbcc1b06946372899a6b88611fa37081
Aditya Bhatraju <48351237+adibns@users.noreply.github.com>
2020-11-21 17:11:22 +0530

    Changed position of optional position parameter

--- demo.R
+++ demo.R
@@ -1,11 +1,13 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Aditya
+# date: 2020-11-21
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [<arga>] [--arg3=<arg3>]
+
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
+[<arga>]          Takes any value (this is a optional positional argument)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
